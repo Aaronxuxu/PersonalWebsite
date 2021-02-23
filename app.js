4d383c2d13f7a667ad8e84e528c20a46cf471f95
Aaron <470164479@qq.com>
2021-02-23 12:45:06 +0800

    小更新

--- app.js
+++ app.js
@@ -8,3 +8,8 @@ const template = require('express-art-template');
 app.engine('art', template);
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'art');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', require('./route/admin'));
+app.listen(80);
+console.log('服务器开启成功');
